4cca0c3e8d0e8e72e94aea0ff2a769496d23d476
Kien Nguyen Trung <kiennt@pivot.vn>
2025-02-12 09:55:20 +0700

    comment wrong test case in unit test

--- backend/test/sum.test.js
+++ backend/test/sum.test.js
@@ -13,8 +13,8 @@ describe('Sum function', () => {
     expect(sum(-1, 1)).toBe(0);
   });
 
-  test('adds 0.1 + 0.2 to equal 0.3', () => {
-    expect(sum(0.1, 0.2)).toBe(0.3);
-  });
+  // test('adds 0.1 + 0.2 to equal 0.3', () => {
+  //   expect(sum(0.1, 0.2)).toBe(0.3);
+  // });
 
 });
